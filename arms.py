c0cf132d5490f1bdc9445c70db242f93139271d7
Preethikumari <47149493+Preethikumari@users.noreply.github.com>
2019-02-02 10:53:32 +0530

    Update arms.py

--- arms.py
+++ arms.py
@@ -1,11 +1,11 @@
 c=int(input())
 d=int(input())
 for a in range(c,d+1):
-	b=0
+	sum=0
 	temp=a
 	while temp>0:
 		digit=temp%10
-		b+=digit**3
+		sum+=digit**3
 		temp//=10
-	if a==b:
+	if a==sum:
 		print(a)
