129223bbc4f9545e5fdc0e8a35fb622a10d98638
Preethikumari <47149493+Preethikumari@users.noreply.github.com>
2019-02-04 21:26:24 +0530

    Update pow.py

--- pow.py
+++ pow.py
@@ -1,8 +1,8 @@
-def power(a,b):
-    if(b==1):
-        return(a)
-    if(b!=1):
-        return(a*power(a,b-1))
-a=int(input())
-b=int(input())
-print(power(a,b))
+def power(y,z):
+    if(z==1):
+        return(y)
+    if(z!=1):
+        return(y*power(y,z-1))
+y=int(input())
+z=int(input())
+print(power(y,z))
